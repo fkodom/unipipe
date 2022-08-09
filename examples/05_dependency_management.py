c3470c9138a4dd956c88caca8363e19b3e5a0427
Frank Odom <frank.odom.iii@gmail.com>
2022-08-09 20:04:57 +0000

    Include inputs used to get the expected outputs

--- examples/05_dependency_management.py
+++ examples/05_dependency_management.py
@@ -11,9 +11,20 @@ def split_name(name: str) -> NamedTuple("Output", first=str, last=str):  # type:
     return names[0], names[-1]
 
 
-@dsl.component
+@dsl.component(
+    # Declare any third-party packages in the 'component' decorator. These are
+    # automatically installed in Docker images for the 'docker' and 'vertex' executors.
+    # NOTE: Currently has no effect for the 'python' executor -- work in progress.
+    packages_to_install=["arrow"],
+)
 def hello(first_name: str, last_name: str) -> str:
-    return f"Seven blessings, {first_name} of house {last_name}!"
+    # All imports must happen *inside* the function!  Modules from the 'unipipe' and
+    # 'typing' modules are automatically imported, but everything else will need to
+    # declared here.
+    import arrow
+
+    today = arrow.now().format("MMMM DD, YYYY")
+    return f"Seven blessings, {first_name} of house {last_name}! Today is {today}."
 
 
 @dsl.component
@@ -48,8 +59,8 @@ if __name__ == "__main__":
 
     # Tested using '--name="Tyrion Lannister"'
     #
-    # # Expected output:
+    # Expected output:
     #
     # INFO:root:[split_name-48b0285e] - ('Tyrion', 'Lannister')
-    # INFO:root:[hello-48b02d2c] - Seven blessings, Tyrion of house Lannister!
+    # INFO:root:[hello-48b02d2c] - Seven blessings, Tyrion of house Lannister! Today is August 09, 2022.
     # INFO:root:[house_motto-48b02e8a] - A Lannister always pays their debts...
