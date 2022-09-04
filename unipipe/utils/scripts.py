cc11a122f785d72114e7fc1f92e576756ce7f256
Frank Odom <frank.odom.iii@gmail.com>
2022-09-04 03:39:36 +0000

    Add utility methods to wrap/run scripts with unipipe

---
+++ unipipe/utils/scripts.py
@@ -0,0 +1,121 @@
+from __future__ import annotations
+
+import ast
+import importlib
+import os
+import sys
+import tempfile
+from itertools import dropwhile
+from typing import Any, Callable, Dict, List, Optional
+
+import unipipe
+from unipipe import dsl
+
+
+def get_docstring_from_script(path: str) -> Optional[str]:
+    with open(path, "r") as f:
+        tree = ast.parse(f.read())
+    return ast.get_docstring(tree, clean=True)
+
+
+def parse_component_kwargs_from_string(decorator: str):
+    decorator = decorator.lstrip("@").lstrip("dsl.").lstrip("component")
+    decorator = decorator.format(**os.environ)
+    return eval(f"dict{decorator}")
+
+
+def get_component_kwargs_from_docstring(docstring: str) -> Optional[Dict[str, Any]]:
+    lines = docstring.split("\n")
+    lines = list(dropwhile(lambda x: not x.startswith("@dsl.component"), lines))
+    lines = [line for line in lines if not line.strip().startswith("#")]
+    if not lines:
+        return None
+
+    exc: Optional[SyntaxError] = None
+    # TODO: Make this cleaner and more robust.
+    # I'm relatively new to Python's 'ast' module, but it seems like it would
+    # be possible to extract the args more elegantly.  This is very brute-force.
+    #
+    # Iteratively add more lines to the string we're trying to parse.  If it
+    # throws a SyntaxError, then add another line and try again.  Otherwise,
+    # we must ahve successfully parsed the component kwargs.
+    for num_lines in range(len(lines)):
+        try:
+            return parse_component_kwargs_from_string("".join(lines[: num_lines + 1]))
+        except SyntaxError as e:
+            exc = e
+
+    assert exc is not None
+    raise exc
+
+
+def get_component_kwargs_from_script(script_path: str) -> Dict[str, Any]:
+    docstring = get_docstring_from_script(script_path)
+    if docstring is None:
+        return {}
+
+    kwargs = get_component_kwargs_from_docstring(docstring)
+    if kwargs is None:
+        kwargs = {}
+
+    return kwargs
+
+
+FUNCTION_NAME = "script_component"
+FUNCTION_CODE = """
+from typing import *
+
+import unipipe
+from unipipe import dsl
+from unipipe.dsl import *
+
+
+def {function_name}(argv: List[str]) -> None:
+    import argparse
+    import sys
+
+    sys.argv = ["{function_name}.py", *argv]
+
+    {script_code}
+"""
+
+
+def function_from_script(script_path: str) -> Callable:
+    name = os.path.basename(script_path).removesuffix(".py")
+    with open(script_path, "r") as f:
+        script_lines = f.readlines()
+    indent = " " * 4
+    code = FUNCTION_CODE.format(
+        function_name=name, script_code=indent.join(script_lines)
+    )
+
+    fp = tempfile.NamedTemporaryFile("w", suffix=".py", delete=False)
+    fp.write(code)
+    fp.flush()
+
+    module_path, file_name = os.path.split(fp.name)
+    sys.path.append(module_path)
+    module = importlib.import_module(file_name.removesuffix(".py"))
+
+    return getattr(module, name)
+
+
+def component_from_script(path: str, **kwargs) -> Callable[..., dsl.Component]:
+    _kwargs = get_component_kwargs_from_script(path)
+    func = function_from_script(path)
+    merged_kwargs = {**_kwargs, **kwargs}
+    return dsl.component(func=func, **merged_kwargs)
+
+
+def run_script(path: str, argv: List[str], executor: str = "python"):
+    component_fn = component_from_script(path=path)
+
+    @dsl.pipeline
+    def pipeline():
+        component_fn(argv=argv)
+
+    unipipe.run(executor=executor, pipeline=pipeline())
+
+
+if __name__ == "__main__":
+    run_script(path="./temp_script.py", argv=["--hello", "world"])
